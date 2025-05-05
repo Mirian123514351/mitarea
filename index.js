a5480292f0b0f09bf0d356afa93b1dfc829149f6
Mirian <mirianvasquez592@gmail.com>
2025-05-05 17:41:37 -0500

    Agregué el servidor web para mostrar el proyecto en el navegador

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo');
+// Importar el módulo HTTP de Node.js
+const http = require('http');
+
+// Crear un servidor web
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>¡Hola Mundo desde Noqqwqde.js en el navegador Con Mirian Vásquez!</h1>');
+});
+
+// Escuchar en el puerto 3000
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
